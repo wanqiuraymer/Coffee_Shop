e71cedc51dbcde254b3d3a871f9a94ba9d9b150f
Wanqiu Raymer <yanwanqiu@gmail.com>
2024-04-11 21:57:37 -0400

    coffee shop

---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers
+{
+
+	public class UsersController : Controller
+	{
+		static List<User> _users = new List<User>
+		{
+			new User
+			{
+				Id = 1,
+				FirstName = "Abby",
+				LastName = "Scotts",
+				Email = "ascotts@gmail.com",
+				Password = "!23$F67E"
+			}
+		};
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View(_users);
+		}
+
+		[HttpGet]
+		public IActionResult Details(int id)
+		{
+			var user = _users.FirstOrDefault(x => x.Id == id);
+			return View(user);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]		
+		public IActionResult Create(User user)
+		{
+			if (ModelState.IsValid)
+			{
+				user.Id = _users.Max(x => x.Id) + 1;
+				_users.Add(user);
+				return RedirectToAction("Welcome", user);
+			}
+			return View(user);
+			
+		}
+		[HttpGet]
+		public IActionResult Welcome(User user) 
+		{ 
+			return View(user);
+		}
+		
+	}
+}
---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegistration.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "First name is required")]
+		[StringLength(50)]
+		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last name is required")]
+		[StringLength(60)]
+		public string LastName { get; set; }
+
+
+		[Required(ErrorMessage = "Email is required")]
+		[EmailAddress(ErrorMessage = "Invalid email address")]
+		public string Email { get; set; }
+
+
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+					   ErrorMessage = "Password must contain at least 8 characters including " +
+			"at least one uppercase letter, one lowercase letter, one digit, and one special " +
+			"character.")]
+		public string Password { get; set; }
+	}
+}
